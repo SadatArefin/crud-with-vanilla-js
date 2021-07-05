5046a6842404f509997bc918d68cbe2ebd976cf2
SadatArefin <sadatarefin@iut-dhaka.edu>
2021-07-05 18:12:56 +0600

    Form to Table Operations

--- src/index.js
+++ src/index.js
@@ -17,3 +17,48 @@ function readFormData() {
     formData["currentCGPA"] = document.getElementById("currentCGPA").value;
     return formData;
 }
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName("tbody")[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.studentId;
+
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.departmentName;
+
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.currentCGPA;
+
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                         <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function validate() {
+    isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    }
+    else {
+        isValid = true;
+        if (
+            !document
+                .getElementById("fullNameValidationError")
+                .classList.contains("hide")
+        )
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("studentId").value = "";
+    document.getElementById("departmentName").value = "";
+    document.getElementById("currentCGPA").value = "";
+    selectedRow = null;
+}
